fc0b71f71ad8e956a040656b1ea38d58fda901b1
Humberto Navarro <a320669@uach.mx>
2021-03-10 13:42:18 -0700

    Se crea el archivo del script principal y el Dockerfile vacio.

---
+++ main.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#vamos a tener varias secciones
+#lo primero que tenemos que hacer es un switch/case o un if para recibir el parametro
+#despues desplegar las opciones de cada parametro (osea metodologias agiles o tradicionales)
+#por ahora vamos a hacer esas cosas
