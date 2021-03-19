f8fdd2c1f7864ef51029ac39edca570c8d8f82a3
Humberto Navarro <a320669@uach.mx>
2021-03-18 17:38:21 -0700

    Se reestructura el codigo, se agregan comentarios, se agregan clears para limpiar la pantalla y se arregla un error relacionado a la metodologia Modelo V

--- main.sh
+++ main.sh
@@ -13,30 +13,31 @@ opcion=""
 opcion2=""
 
 #Arreglos con los nombres para cada tipo de metodologia
-nombresAgiles=('SCRUM' 'X.P.' 'Kanban' 'Crystal')
-nombresTradicionales=('Cascada' 'Espiral' 'Modelo V')
+nombresAgiles=('SCRUM' 'X.P' 'Kanban' 'Crystal')
+nombresTradicionales=('Cascada' 'Espiral' 'Modelo-V')
 
 ###############################################################################
 #######################Inicio del codigo del script############################
-		##############-METODOLOGIAS AGILES-#################
+##############-METODOLOGIAS AGILES-#################
 case $1 in
-  -a|-A) #Condicion
-  until [[ $opcion == "5" ]]; do
-    #statements
-  echo "Bienvenido a la guía rápida de Agile, para continuar seleccione un tema:
+    -a|-A) #Condicion
+        until [[ $opcion == "5" ]]; do
+            #Imprimimos el menu de metodologias agiles
+            echo "Bienvenido a la guía rápida de Agile, para continuar seleccione un tema:
 
             1.-SCRUM
             2.-X.P.
             3.-Kanban
             4.-Crystal
             5.-Exit
-"
+            "
 
-read -p "Ingrese la opcion: " opcion
-
-	case $opcion in
-     	1|2|3|4)
-    echo "Usted esta en la sección de ${nombresAgiles[opcion - 1]}, seleccione la opción que desea utilizar.
+            read -p "Ingrese la opcion: " opcion
+            clear
+            case $opcion in
+                1|2|3|4)
+                #Imprimimos el menu de cada seccion individual dependiendo de la opcion elegida
+                echo "Usted esta en la sección de ${nombresAgiles[opcion - 1]}, seleccione la opción que desea utilizar.
 
             1.-Agregar información
             2.-Buscar
@@ -44,88 +45,100 @@ read -p "Ingrese la opcion: " opcion
             4.-Leer base de información.
             5.-Regresar
 
-"
+                    "
                     read -p "Ingrese la opcion: " opcion2
+                    clear
                     case $opcion2 in
-                      1)
-			#Creamos el archivo necesario (si ya existe el archivo esta instruccion no pasa nada)
-                        touch ${nombresAgiles[opcion-1]}.inf
+                        1)  #OPCION DE AGREGAR INFORMACION
+                            #Creamos el archivo necesario (si ya existe el archivo esta instruccion no pasa nada)
+                            touch ${nombresAgiles[opcion-1],,}.inf
 
-                        #Captura el titulo y la definicion del concepto a agregar
-                        read -p "Introduzca el titulo del concepto: " titulo
-                        echo ''
-                        read -p "Agregue la definicion del concepto: " descripcion
+                            #Captura el titulo y la definicion del concepto a agregar
+                            read -p "Introduzca el titulo del concepto: " titulo
+                            echo ''
+                            read -p "Agregue la definicion del concepto: " descripcion
 
-                        #Agregamos la informacion solicitada en el formato apropiado al documento
-                        echo "[$titulo].- $descripcion" >> ./${nombresAgiles[opcion-1]}.inf
-                        echo ''
-                        echo ''
+                            #Agregamos la informacion solicitada en el formato apropiado al documento
+                            echo "[$titulo].- $descripcion" >> ./${nombresAgiles[opcion-1],,}.inf
+                            echo ''
+                            echo ''
 
-                         ;;
-                       2)
-                             #Capturamos la palabra o concepto que queremos buscar
-                        read -p "Introduzca el concepto que desea buscar: " buscar
-                          #statements
-                          if [ -f ./${nombresAgiles[opcion-1]}.inf ]
+                            ;;
+                        2)  #OPCION DE BUSCAR
+                            #Verificamos si el archivo de la metodologia ha sido creado
+                            if [ -f ./${nombresAgiles[opcion-1],,}.inf ]
                             then
-                              echo "Estos son los resultados encontrados: "
-                              echo ''
-                              grep -i $buscar ./${nombresAgiles[opcion-1]}.inf
-                              echo ''
+                                #Capturamos la palabra o concepto que el usuario quiere buscar
+                                read -p "Introduzca el concepto que desea buscar: " buscar
+
+                                #Buscamos e imprimimos lo que encontramos
+                                echo "Estos son los resultados encontrados: "
+                                echo ''
+                                grep -i $buscar ./${nombresAgiles[opcion-1],,}.inf
+                                echo ''
 
                             else
-                              echo "El archivo no existe"
+                                echo "El archivo no existe"
                             fi
 
-                        #Buscamos e imprimimos lo que encontramos
-                         ;;
-                       3)
-                        if [ -f ./${nombresAgiles[opcion-1]}.inf ]
-                         then
-                           rm ./${nombresAgiles[opcion-1]}.inf
-                          echo "El archivo de ${nombresAgiles[opcion-1]} a sido eliminado con exito"
-                         else
-                           echo "El archivo no existe"
-                         fi
-                         ;;
-                       4)
-                       if [ -f ./${nombresAgiles[opcion-1]}.inf ]
-                        then
-                          cat ${nombresAgiles[opcion-1]}.inf
-                        else
-                          echo "El archivo no existe"
-                        fi
-                        #Imprimir el archivo solicitado
+                            ;;
+                        3)  #OPCION DE ELIMINAR INFORMACION
+                            #Verificamos si el archivo de la metodologia ha sido creado
+                            if [ -f ./${nombresAgiles[opcion-1],,}.inf ]
+                            then
+                                rm ./${nombresAgiles[opcion-1],,}.inf
+                                echo "El archivo de ${nombresAgiles[opcion-1]} a sido eliminado con exito"
+                            else
+                                echo "El archivo no existe"
+                            fi
+                            ;;
+                        4)  #OPCION DE IMPRIMIR EL ARCHIVO
+                            #Verificamos si el archivo de la metodologia ha sido creado
+                            if [ -f ./${nombresAgiles[opcion-1],,}.inf ]
+                            then
+                                #Imprimimos el archivo de la metodologia que contiene la informacion
+                                echo "Esta es la informacion disponible hasta ahora: "
+                                echo ''
+                                cat ${nombresAgiles[opcion-1],,}.inf
+                                echo''
+                            else
+                                echo "El archivo no existe"
+                            fi
 
 
 
 
-			;;
+
+                            ;;
 
                     esac
-	;;
-	esac
+                    ;;
+            esac
 
 
 
-		##############-METODOLOGIAS TRADICIONALES-#################
-done
-;;
+            ##############-METODOLOGIAS TRADICIONALES-#################
+        done
+        ;;
     -t|-T)
-      until [[ $opcion == "4" ]]; do
-    echo "Bienvenido a la guía rápida de metodologias Tradicionales, para continuar seleccione un tema:
+        until [[ $opcion == "4" ]]; do
+            #Imprimimos el menu de metodologias tradicionales
+            echo "Bienvenido a la guía rápida de metodologias Tradicionales, para continuar seleccione un tema:
 
               1.-Cascada
               2.-Espiral
               3.-Modelo V
               4.-Salir
 
-  "
-  read -p "Ingrese la opcion: " opcion
+            "
+            read -p "Ingrese la opcion: " opcion
+            clear
 
-	case $opcion in
-     	1|2|3)
-    echo "Usted esta en la sección de ${nombresTradicionales[opcion - 1]}, seleccione la opción que desea utilizar.
+            case $opcion in
+                1|2|3)
+                  #Imprimimos el menu de cada seccion individual dependiendo de la opcion elegida
+                  clear
+                  echo "Usted esta en la sección de ${nombresTradicionales[opcion - 1]}, seleccione la opción que desea utilizar.
 
             1.-Agregar información
             2.-Buscar
@@ -133,67 +146,77 @@ done
             4.-Leer base de información.
             5.-Regresar
 
-"
+                    "
                     read -p "Ingrese la opcion: " opcion2
+                    clear
                     case $opcion2 in
-                       1)
-			#Creamos el archivo necesario (si ya existe el archivo esta instruccion no pasa nada)
-			touch ${nombresTradicionales[opcion-1]}.inf
+                      1)  #OPCION DE AGREGAR INFORMACION
+                          #Creamos el archivo necesario (si ya existe el archivo esta instruccion no pasa nada)
+                          touch ${nombresTradicionales[opcion-1],,}.inf
 
-			#Captura el titulo y la definicion del concepto a agregar
-			read -p "Introduzca el titulo del concepto: " titulo
-			echo ''
-			read -p "Agregue la definicion del concepto: " descripcion
+                          #Captura el titulo y la definicion del concepto a agregar
+                          read -p "Introduzca el titulo del concepto: " titulo
+                          echo ''
+                          read -p "Agregue la definicion del concepto: " descripcion
 
-			#Agregamos la informacion solicitada en el formato apropiado al documento
-			echo "[$titulo].- $descripcion" >> ./${nombresTradicionales[opcion-1]}.inf
-			echo ''
-			echo ''
+                          #Agregamos la informacion solicitada en el formato apropiado al documento
+                          echo "[$titulo].- $descripcion" >> ./${nombresTradicionales[opcion-1],,}.inf
+                          echo ''
+                          echo ''
 
-                         ;;
-                       2)
-			#Capturamos la palabra o concepto que queremos buscar
-                        read -p "Introduzca el concepto que desea buscar: " buscar
-                        if [ -f ./${nombresTradicionales[opcion-1]}.inf ]
-                          then
-                            echo "Estos son los resultados encontrados: "
-                            echo ''
-                            grep -i $buscar ./${nombresTradicionales[opcion-1]}.inf
-                            echo ''
+                          ;;
+                      2)  #OPCION DE BUSCAR
+                          #Verificamos si el archivo de la metodologia ha sido creado
+                          if [ -f ./${nombresTradicionales[opcion-1],,}.inf ]
+                           then
+                              #Capturamos la palabra o concepto que el usuario quiere buscar
+                              read -p "Introduzca el concepto que desea buscar: " buscar
+
+                              #Buscamos e imprimimos lo que encontramos
+                              echo "Estos son los resultados encontrados: "
+                              echo ''
+                              grep -i $buscar ./${nombresTradicionales[opcion-1],,}.inf
+                              echo ''
 
                           else
-                            echo "El archivo no existe"
+                              echo "El archivo no existe"
+                          fi
+
+                          ;;
+                      3)  #OPCION DE ELIMINAR INFORMACION
+                          #Verificamos si el archivo de la metodologia ha sido creado
+                          if [ -f ./${nombresTradicionales[opcion-1],,}.inf ]
+                          then
+                              rm ./${nombresTradicionales[opcion-1],,}.inf
+                              echo "El archivo de ${nombresTradicionales[opcion-1]} a sido eliminado con exito"
+                          else
+                              echo "El archivo no existe"
+                          fi
+                          ;;
+                      4)  #OPCION DE IMPRIMIR EL ARCHIVO
+                          #Verificamos si el archivo de la metodologia ha sido creado
+                          if [ -f ./${nombresTradicionales[opcion-1],,}.inf ]
+                          then
+                          #Imprimimos el archivo de la metodologia que contiene la informacion
+                              echo "Esta es la informacion disponible hasta ahora: "
+                              echo ''
+                              cat ${nombresTradicionales[opcion-1],,}.inf
+                              echo''
+                          else
+                              echo "El archivo no existe"
                           fi
 
 
-                         ;;
-                       3)
-                       if [ -f ./${nombresTradicionales[opcion-1]}.inf ]
-                        then
-                          rm ./${nombresTradicionales[opcion-1]}.inf
-                         echo "El archivo de ${nombresTradicionales[opcion-1]} a sido eliminado con exito"
-                        else
-                          echo "El archivo no existe"
-                        fi
 
 
-                         ;;
-                       4)
-                        #Imprimir el archivo solicitado
-                        if [ -f ./${nombresTradicionales[opcion-1]}.inf ]
-                         then
-                           cat ${nombresTradicionales[opcion-1]}.inf
-                         else
-                           echo "El archivo no existe"
-                         fi
-			 ;;
+                          ;;
 
                     esac
-	;;
-	esac
-  done
-    ;;
+                    ;;
+            esac
+        done
+        ;;
     *)
-    echo "No es una opcion valida"
-    ;;
+        echo "No es una opcion valida"
+        ;;
 esac
